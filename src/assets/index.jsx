c199da3e46d47b09b6ac1f48114e53cf446c1f42
Yohan910 <yohan.dacosta910@gmail.com>
2025-04-01 20:26:21 +0200

    Created image file

---
+++ src/assets/index.jsx
@@ -0,0 +1,3 @@
+import basel_img from "./img/basel.png";
+
+export { basel_img };
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,12 +1,33 @@
 import Card from "./Card";
 import NoCourses from "../common/NoCourses";
 import useFilteredCourses from "../../hooks/useFilteredCourses";
+import { basel_img } from "../../assets";
 
 const Main = () => {
   const { filtered } = useFilteredCourses();
 
   return (
-    <div className="w-full md:w-3/4 space-y-6 px-4 sm:px-0">
+    <div className="flex-col w-full md:w-3/4 space-y-6 px-4 sm:px-0">
+      <div className="flex flex-col space-y-4 bg-gray-100 p-4 md:flex-row md:h-[14rem] md:justify-center md:rounded-full md:p-0">
+        <div className="flex flex-1">
+          <div className="flex flex-col justify-center mx-auto space-y-1">
+            <h1 className="text-lg font-semibold">Deutsch für Basel</h1>
+            <p className="text-md font-semibold text-gray-500">
+              Wollen Sie Deutsch lernen?
+            </p>
+            <span className="text-xs font-semibold text-gray-400">
+              Suchen Sie ein Kurs in Basel-Stadt?
+            </span>
+          </div>
+        </div>
+        <div className="flex">
+          <img
+            className="rounded-full shadow-lg shadow-green-100"
+            src={basel_img}
+            alt="basel_courses"
+          />
+        </div>
+      </div>
       {filtered.length <= 0 ? (
         <NoCourses />
       ) : (
